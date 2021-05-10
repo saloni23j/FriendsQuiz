cd18da50078c5b64b3e7b423e72330315265a7ed
saloni23j <55657758+saloni23j@users.noreply.github.com>
2021-05-10 21:45:29 +0530

    added more questions

--- Friends.js
+++ Friends.js
@@ -23,10 +23,45 @@ var questions = [{
 },{
   question: "What is the name of Rachel's hairless cat?",
 answer: "Mrs. Whiskerson"
+},
+{
+  question: "On the episode 'the one with the truth about London', with who Monica wanted to stay in first place?",
+  answer: "Joey"
+},
+{
+  question: "Joey had an imaginary childhood friend. His profession was?",
+  answer: "Space cowboy"
+},
+{
+  question: 'Who said: "Pivot. Pi-vot. PI-VOT."',
+  answer: "Ross"
+},
+{
+  question: "Who said: 'Welcome to the real world. It sucks. You're gonna love it'",
+  answer: "Monica"
+},
+{
+  question: "What was the name of Chandler's and Ross's band?",
+  answer: "Way, no way"
+},
+{
+  question: "What is the fake name Joey used to have?",
+  answer: "Ken Adams "
+},
+{
+  question: 'On the episode "the one with the male nanny" what was the name of the nanny Rachel and Ross hired?',
+  answer: "Sandy"
 }
 ]
 for(var i = 0 ; i < questions.length; i++){
   play(questions[i].question, questions[i].answer);
 }
-console.log("Your final score is: " +score); 
-// console.log("hello world");
+
+if(score > 8)
+{
+  console.log("Your really are a big fan. You Scored : " +score); 
+
+}
+else{
+  console.log("nahhhh! U just brag about being a huge fan");
+}
